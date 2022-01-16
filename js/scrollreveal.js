a37dd555059078b11666f735a087fd094b118fa8
carloh.js <87372655+c4rlohjs@users.noreply.github.com>
2022-01-16 20:55:56 -0300

    Add files via upload

---
+++ js/scrollreveal.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.topnav', {
+    duration: 1500
+});
+
+ScrollReveal().reveal('.main-page', {
+    duration: 1000
+});
+
+ScrollReveal().reveal('.game-category', {
+    duration: 1000
+});
+
+ScrollReveal().reveal('.plans-div', {
+    duration: 1000
+});
+
+ScrollReveal().reveal('.main-fivem', {
+    duration: 2000
+});
